bdf71b44064e0d5346d92eecf4992eeabde4d103
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-07 17:02:22 +0100

    fix argv[0] = str[0]

--- verify_path.c
+++ verify_path.c
@@ -98,7 +98,7 @@ int verify_path(int count, char **str)
 		free(path);
 		return (-1);
 	}
-	argv[0] = path;
+	argv[0] = str[0];
 	for (i = 1; i < count; i++)
 		argv[i] = str[i];
 	argv[count] = NULL;
