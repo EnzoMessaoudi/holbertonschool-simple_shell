ac45f06cc52d080bb68d5e0af35746b9c512b52d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-08 14:51:19 +0100

    ADD variable last_result, that stock the process of the last command

--- shell.c
+++ shell.c
@@ -50,7 +50,7 @@ int simple_shell(char *shell_name)
 {
 	char *comm = NULL, *args[30];
 	size_t size = 0, len;
-	int i = 1, count = 0, result = 0;
+	int i = 1, count = 0, result = 0, last_result = 0;
 
 	while (1)
 	{
@@ -77,12 +77,14 @@ int simple_shell(char *shell_name)
 		if (strcmp(args[0], "exit") == 0)
 		{
 			free(comm);
-			exit(result);
+			exit(last_result);
 		}
 		result = verify_env(count, args);
+		last_result = result;
 		if (handle_success(result, &i) == 0)
 			continue;
 		result = verify_path(count, args);
+		last_result = result;
 		if (handle_success(result, &i) == 0)
 			continue;
 		printf("%s: %d: %s : not found\n", shell_name, i++, args[0]);
