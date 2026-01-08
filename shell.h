5b05a44f0705ca4a2b6d603d57df2bce4f0b0d7c
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-08 10:53:12 +0100

    Remove testing prototypes

--- shell.h
+++ shell.h
@@ -14,20 +14,12 @@ extern char **environ;
 int simple_shell(char *shell_name);
 int verify_path(int count, char **str);
 int verify_env(int count, char **str);
-int _av(int argc, char *argv[]);
-int read_line(void);
-int _strtok(void);
 void _printenv(void);
-int env_v_prenv(int argc, char **argv, char **env);
-char *_getenv(const char *name);
-void print_path_dirs(void);
 int _setenv(const char *name, const char *value, int overwrite);
 int _unsetenv(const char *name);
-int fork_wait_execve(void);
-int super_simple_shell(void);
-int file_exists(const char *path);
 int _getpid(void);
 char *find_path(char *av);
+pid_t fork_and_exec(char *path, char **argv);
 
 
 # endif
