e5f393a44ffd209a738ba27583b81596dfd3f13d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-09 08:55:50 +0100

    ADD verify_env_functions.c

---
+++ verify_env_functions.c
@@ -0,0 +1,102 @@
+#include "shell.h"
+
+/**
+ * _printenv - Prints all environment variables
+ *
+ * Return: 0 on success
+ */
+
+void _printenv(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
+
+/**
+ * _setenv - Adds or modifies an environment variable
+ * @name: Name of the variable
+ * @value: Value to set
+ * @overwrite: If non-zero, overwrite existing variable
+ *
+ * Return: 0 on success, -1 on failure
+ */
+
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	size_t i = 0, len, j;
+	char *str, **new_env;
+
+	if (name == NULL || value == NULL)
+		return (-1);
+	len = strlen(name) + strlen(value) + 2;
+	str = malloc(len);
+	if (str == NULL)
+		return (-1);
+	sprintf(str, "%s=%s", name, value);
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], name, strlen(name)) == 0 &&
+			environ[i][strlen(name)] == '=')
+		{
+			if (overwrite == 0)
+			{
+				free(str);
+				return (0);
+			}
+			free(environ[i]);
+			environ[i] = str;
+			return (0);
+		}
+		i++;
+	}
+	new_env = malloc(sizeof(char *) * (i + 2));
+	if (new_env == NULL)
+	{
+		free(str);
+		return (-1);
+	}
+	for (j = 0; j < i; j++)
+		new_env[j] = environ[j];
+	new_env[i] = str;
+	new_env[i + 1] = NULL;
+	if (environ != NULL)
+		free(environ);
+	environ = new_env;
+	return (0);
+}
+
+/**
+* _unsetenv - Function that remove a environnement variable
+* @name: Name of the variable we want to remove
+* Return: 0 on success or -1 on failure
+*/
+
+int _unsetenv(const char *name)
+{
+	size_t i = 0, j;
+	size_t len;
+
+	if (name == NULL)
+		return (-1);
+
+	len = strlen(name);
+
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+		{
+			free(environ[i]);
+			for (j = i; environ[j] != NULL; j++)
+				environ[j] = environ[j + 1];
+
+			return (0);
+		}
+		i++;
+	}
+	return (0);
+}
